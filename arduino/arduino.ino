eb84b3a3b9a962df8183c2e1d7bd6dedd38aede6
Felix Weichselgartner <felix.weichselgartner@t-online.de>
2020-11-26 10:22:53 +0100

    PORT mode for faster reading

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,6 +1,27 @@
 #define AMOUNT_PINS 2
 
+#define FAST_MODE
+
+#ifdef FAST_MODE
+int pins[] = {
+    24, 25, 26, 27, 28, 29, 30, 31,                                 // data
+    34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, // address
+    51, 52, 53                                                      // rest
+};
+// https://www.arduino.cc/en/Hacking/PinMapping2560
+// 22 - 29 -> PortA 0 - 7
+// 30 - 37 -> PortC 7 - 0
+// 38      -> PortD 7
+// 39 - 41 -> PortG 2 - 0
+// 42 - 49 -> PortL 7 - 0
+// 50 - 53 -> PortK 3 - 0
+#else
 int pins[] = {4, 7};
+#endif
+
+// in microseconds
+#define SAMPLING_INTERVAL 10
+
 
 void setup()
 {
@@ -14,9 +35,22 @@ void setup()
 void loop()
 {
 
-    if (micros() % 10 == 0)
+    if (micros() % SAMPLING_INTERVAL == 0)
     {
         String data = "";
+        
+        #ifdef FAST_MODE
+        
+        // make evulation on needed pins on receiver side.
+        data += (String)PORTA + "; ";
+        data += (String)PORTC + "; ";
+        data += (String)PORTD + "; ";
+        data += (String)PORTG + "; ";
+        data += (String)PORTL + "; ";
+        data += (String)PORTK;
+        
+        #else
+        
         for (int i = 0; i < AMOUNT_PINS; i++)
         {
             data += (String)digitalRead(pins[i]);
@@ -25,6 +59,9 @@ void loop()
                 data += "; ";
             }
         }
+        
+        #endif
+
         Serial.println(data);
     }
 }
